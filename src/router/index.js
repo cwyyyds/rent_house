7f02588ac64c7158d03b138ccfc0b72b2fb51f54
cwyyyds <1013953614@qq.com>
2022-07-13 19:39:58 +0800

    初始化2

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
@@ -7,7 +8,7 @@ const routes = [
   {
     path: '/login',
     name: 'login',
-    component: () => import('@/views/Login')
+    component: HomeView
   }
 ]
 
